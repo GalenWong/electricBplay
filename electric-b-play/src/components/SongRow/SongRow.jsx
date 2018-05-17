a85a3327a57c0560a54b1c240d2ac5144c10df17
GalenWong <wonggalen1999@gmail.com>
2018-05-16 23:26:14 -0700

    initial commit

---
+++ electric-b-play/src/components/SongRow/SongRow.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { Grid, Col, Row } from 'react-bootstrap';
+import { PropTypes } from 'prop-types';
+
+const descriptionStyle = {
+    textAlign: "left",
+};
+
+const titleStyle={
+    fontSize:'1.3em',
+};
+
+const artistStyle={
+
+};
+
+class SongRow extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            selected:false,
+        };
+    }
+
+    static get PropTypes() {
+        return {
+            songName: PropTypes.string.isRequired,
+            artistName: PropTypes.string.isRequired,
+            duration: PropTypes.string.isRequired
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                <Col md={1}>
+
+                </Col>
+                <Col md={9} style={descriptionStyle}>
+                    <Row style = {titleStyle}>
+                        <p>All Falls Down</p>
+                    </Row>
+                    <Row>
+                        <p>J.Fla</p>
+                    </Row>
+                </Col>
+                <Col md={1}>
+                </Col>
+                <Col md={1}>
+                    3:00
+                </Col>
+            </div>
+        );
+    }
+
+}
+
+export default SongRow;
